b0ee14e876e1c0c8c2ac5b94bff3cc1223de982c
putra <zainudin.saputra@dkatalis.com>
2022-10-02 01:50:49 +0700

    fix error after move worker file

--- src/common/worker/getCsvStringFromObjects.worker.ts
+++ src/common/worker/getCsvStringFromObjects.worker.ts
@@ -2,7 +2,7 @@ import { parentPort, workerData } from "worker_threads";
 import { getCsvStringFromObjects } from "../utils";
 
 export const CSV_STRING_FROM_OBJECTS_WORKER_PATH =
-  "../common/getCsvStringFromObjects.worker.js";
+  "../common/worker/getCsvStringFromObjects.worker.js";
 
 const getCsvStringFromObjectsWorker = (): string => {
   const objects: Object[] = workerData.objects;
